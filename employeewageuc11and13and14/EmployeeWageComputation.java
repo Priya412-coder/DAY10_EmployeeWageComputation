f035b70b6b3de87458f750c2da47e481834251c5
Priya412-coder <priyankasingh8249@gmail.com>
2021-12-14 00:15:20 +0530

    adding UC11 , UC13 and UC14 of Employee wage computation

---
+++ employeewageuc11and13and14/EmployeeWageComputation.java
@@ -0,0 +1,57 @@
+package com.employeewageuc11and13and14;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeWageComputation implements IEmployeeWage {
+	String empName;
+	List<Company> companies;
+	Map<String, Company> companiesMap;
+	
+	public EmployeeWageComputation(String empName) {
+		this.empName = empName;
+		companies = new LinkedList<Company>();
+		companiesMap = new HashMap<String, Company>();
+	}
+
+	public void addCompany(Company company) {
+		companies.add(company);
+		calculateAllCompaniesEmpWage();
+	}
+
+	public void calculateAllCompaniesEmpWage() {
+		for (Company company : companies) {
+			if(!companiesMap.containsKey(company.companyName)) {
+				company.calculateEmpWage();
+				companiesMap.put(company.companyName, company);	
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		String str = "";
+		for (Company company : companies) {
+			if (company == null) {
+				break;
+			}
+			str = str.concat("\n-----------------------------------\n"+company);
+		}
+		return str;
+	}
+	
+	
+
+	public int getTotalWage(String companyName) {
+		return companiesMap.get(companyName).totalEmpWage;
+	}
+
+	public Company getCompany(int index) {
+		return companies.get(index);
+	}
+
+	public Company getCompany(String companyName) {
+		return companiesMap.get(companyName);
+	}
+}
---
+++ employeewageuc11and13and14/IEmployeeWage.java
@@ -0,0 +1,11 @@
+package com.employeewageuc11and13and14;
+
+public interface IEmployeeWage {
+public void addCompany(Company company);
+	
+	public int getTotalWage(String companyName);
+	
+	public Company getCompany(int index);
+	
+	public Company getCompany(String companyName);
+}
